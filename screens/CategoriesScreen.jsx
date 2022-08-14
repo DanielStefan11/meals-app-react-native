9ffe2eac1c608aedeaeb5cccdd58a048a6b826c7
DanielStefan11 <dannielstefan592@gmail.com>
2022-08-14 13:33:16 +0300

    react native navigation

--- screens/CategoriesScreen.jsx
+++ screens/CategoriesScreen.jsx
@@ -2,11 +2,13 @@ import { FlatList, StyleSheet } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-const renderCategoryItem = itemData => {
-  return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />;
-};
+const CategoriesScreen = ({ navigation }) => {
+  const renderCategoryItem = itemData => {
+    const pressHandler = () => navigation.navigate("MealsOverview");
 
-const CategoriesScreen = () => {
+    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler} />;
+  };
+
   return <FlatList data={CATEGORIES} keyExtractor={item => item.id} renderItem={renderCategoryItem} numColumns={2} />;
 };
 
