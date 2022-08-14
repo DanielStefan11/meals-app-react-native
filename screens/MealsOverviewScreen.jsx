62cd4cef55f8a49f9c6a2b9eeeb25e946092fe4d
DanielStefan11 <dannielstefan592@gmail.com>
2022-08-14 21:07:34 +0300

    set dynamic navigation

--- screens/MealsOverviewScreen.jsx
+++ screens/MealsOverviewScreen.jsx
@@ -1,13 +1,22 @@
+import { useEffect, useLayoutEffect } from "react";
 import { View, StyleSheet, FlatList } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 
-const MealsOverview = ({ route }) => {
+const MealsOverviewScreen = ({ route, navigation }) => {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter(mealItem => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(category => category.id === catId).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
 
   const renderMealItem = itemData => {
     const item = itemData.item;
@@ -30,7 +39,7 @@ const MealsOverview = ({ route }) => {
   );
 };
 
-export default MealsOverview;
+export default MealsOverviewScreen;
 
 const styles = StyleSheet.create({
   container: {
